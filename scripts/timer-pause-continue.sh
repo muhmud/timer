4a6b63cdc24be214de3f2a364509bb127538c3c5
Muhmud Ahmad <muhmud.ahmad@googlemail.com>
2018-03-05 12:57:50 +0800

    updated scripts

--- scripts/timer-pause-continue.sh
+++ scripts/timer-pause-continue.sh
@@ -4,7 +4,7 @@ TIMER=~/.timer/current
 
 if [[ -f $TIMER ]]; then
   TIMER_DIRECTORY=$(cat $TIMER)
-  TIMER_STATUS=$(timer --status -D "$TIMER_DIRECTORY")
+  TIMER_STATUS=$(~/bin/timer --status -D "$TIMER_DIRECTORY")
   STATUS=$(echo $TIMER_STATUS | jq --raw-output '.timer.current.status')
   
   if [[ "$STATUS" != "STOPPED" ]]; then
@@ -14,7 +14,7 @@ if [[ -f $TIMER ]]; then
       ~/bin/timer --resume -D "$TIMER_DIRECTORY"
     fi
   else
-    TASK_DESCRIPTION=$(zenity --entry --width=500 --title="Start Timer (`basename $TIMER_DIRECTORY`)" --text="Enter Task Description:" --ok-label="Start")
+    TASK_DESCRIPTION=$(eval "zenity --entry --width=500 --title 'Start Timer' --text 'Enter Task Description:' --ok-label='Start' $(~/bin/timer -D "$TIMER_DIRECTORY" --report task --start-date $(date +'%Y-%m-%d' -d '3 months ago') --end-date $(date +'%Y-%m-%d' -d 'next month') | awk -F "\t" '{ gsub(/[ \t]+$/, "", $1); print $1 }' | sort -u | (tasks="";while read t; do tasks="$tasks '$t'"; done;echo $tasks))")
     if [[ ! -z $TASK_DESCRIPTION ]]; then
       ~/bin/timer --go -T "$TASK_DESCRIPTION" -D "$TIMER_DIRECTORY"
     fi
--- scripts/timer-status.sh
+++ scripts/timer-status.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 TIMER=~/.timer/current
+TIMER_DIRECTORY=$(cat $TIMER)
+
+get_details() {  
+  TIMER_STATUS=$(~/bin/timer --status -D "$TIMER_DIRECTORY")
+  PROJECT=$(echo $TIMER_STATUS | jq --raw-output '.project' 2>/dev/null)
+  STATUS=$(echo $TIMER_STATUS | jq --raw-output '.timer.current.status' 2>/dev/null)
+  WORK_DONE=$(echo $TIMER_STATUS | jq --raw-output '.timer.workDone' 2>/dev/null)
+}
 
 if [[ -f $TIMER ]]; then
-  TIMER_DIRECTORY=$(cat $TIMER)
-  TIMER_STATUS=$(timer --status -D "$TIMER_DIRECTORY")
-  PROJECT=$(echo $TIMER_STATUS | jq --raw-output '.project')
-  STATUS=$(echo $TIMER_STATUS | jq --raw-output '.timer.current.status')
-  WORK_DONE=$(echo $TIMER_STATUS | jq --raw-output '.timer.workDone')
+  get_details
+  if [[ "$PROJECT" == "" ]]; then
+    sleep 1
+    get_details
+  fi
 
   if [[ "$STATUS" == "RUNNING" ]]; then
       echo " $PROJECT - %{F#f00}$WORK_DONE%{F-}"
